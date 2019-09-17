92cf2183e065cd44436b273dde4a61598c4c876d
Raúl Francés <raulfrancesm1997@gmail.com>
2019-09-17 23:58:24 +0200

    :new: Assets organizados, escenas creadas

---
+++ Devs Jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        Application.targetFrameRate = 300;
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+
+        LoadData();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveData();
+    }
+
+    //-------------------- FUNCTIONS --------------------
+
+    //Called to start de game
+    public void InitGame()
+    {
+    }
+
+    //Called to start de game
+    public void StartGame()
+    {
+    }
+
+    //Called when the player die
+    public void GameOver()
+    {
+    }
+
+    public void QuitGame()
+    {
+        SaveData();
+        Application.Quit();
+    }
+
+    //Called when the player decide to go back to the menu
+    public void BackToMenu()
+    {
+    }
+
+    public void LoadScene(string nameLevel)
+    {
+        SceneManager.LoadScene(nameLevel);
+    }
+
+    private IEnumerator LoadAsyncScene(string nameLevel)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nameLevel);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+
+    public void SaveData()
+    {
+
+        PlayerPrefs.Save();
+
+    }
+
+    public void LoadData()
+    {
+        PlayerPrefs.GetString("");
+    }
+
+    public void ChangeData()
+    {
+        SaveData();
+        LoadData();
+    }
+
+    public void loadDialogo()
+    {
+        LoadScene("Dialogo");
+    }
+
+    public void loadInvestigacion()
+    {
+        LoadScene("Investigacion");
+    }
+
+    public void loadMenuPrincipal()
+    {
+        LoadScene("Menu Principal");
+    }
+
+    public void loadMundo()
+    {
+        LoadScene("Mundo");
+    }
+}
