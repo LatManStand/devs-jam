920a1200c485727aa2c4a3f11747dd897fb9ccd9
Alex <alejandro.gemas@gmail.com>
2019-09-22 15:44:47 +0200

    Pequeñas intros en las escenas

---
+++ Devs Jam/Assets/Scripts/OficinaManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OficinaManager : MonoBehaviour
+{
+    public GameObject conversacionJefe;
+    public GameObject titulo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameManager.instance.getDia() == 1)
+        {
+            StartCoroutine(Animation());
+        }
+    }
+
+    IEnumerator Animation()
+    {
+        yield return new WaitForSeconds(0.3f);
+        titulo.SetActive(true);
+        yield return new WaitForSeconds(3.7f);
+        titulo.SetActive(false);
+    }
+}
