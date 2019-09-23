32ec191d2096ea4f1145adfeb0f51597b477c9be
Alejandro <alejandro.gemas@gmail.com>
2019-09-23 12:37:54 +0200

    Animacion Prota

--- Devs Jam/Assets/Scripts/PlayerController.cs
+++ Devs Jam/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     SpriteRenderer spr;
+    Animator anim;
+    private bool direccion;
 
     private void Start()
     {
+        direccion = false;
         spr = this.gameObject.GetComponent<SpriteRenderer>();
+        anim = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,18 +20,43 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.anyKey)
         {
-            if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && gameObject.transform.position.x >-21) 
+            if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && gameObject.transform.position.x >-21)
             {
+                anim.SetBool("IdleDer", false);
+                anim.SetBool("IdleIzq", false);
+                direccion = false;
                 spr.flipX = true;
                 this.gameObject.transform.position += new Vector3(-2, 0, 0) * Time.deltaTime;
+                anim.SetBool("Izq", true);
+                anim.SetBool("Der", false);
             }
 
             if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && gameObject.transform.position.x < 11)
             {
+                anim.SetBool("IdleDer", false);
+                anim.SetBool("IdleIzq", false);
+                direccion = true;
                 spr.flipX = false;
                 this.gameObject.transform.position += new Vector3(2, 0, 0) * Time.deltaTime;
+                anim.SetBool("Izq", false);
+                anim.SetBool("Der", true);
             }
 
+        }
+        else
+        {
+            anim.SetBool("Izq", false);
+            anim.SetBool("Der", false);
+            if (direccion)
+            {
+                anim.SetBool("IdleDer", true);
+                anim.SetBool("IdleIzq", false);
+            }
+            else
+            {
+                anim.SetBool("IdleIzq", true);
+                anim.SetBool("IdleDer", false);
+            }
         }
         
     }
